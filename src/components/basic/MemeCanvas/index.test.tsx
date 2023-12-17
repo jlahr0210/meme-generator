da1a52392f537532d4474dad445de85cee6bcfab
Jeff Lahr <jlahr@redeapp.com>
2023-12-17 09:57:05 -0600

    use actual image in test

--- src/components/basic/MemeCanvas/index.test.tsx
+++ src/components/basic/MemeCanvas/index.test.tsx
@@ -2,7 +2,8 @@ import { render, screen } from '@testing-library/react';
 import { describe, expect, it } from 'vitest';
 import { MemeCanvas } from '.';
 
-const url = 'https://ibb.co/jhsW1sR';
+const url =
+  'https://i.ibb.co/nrtn5tC/Hill-Grant-99-MEDR8400-crop-north.jpg';
 
 /* simple image source test */
 describe('Image Source Testing', () => {
